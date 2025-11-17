e61977ef29bd800f8ed57f8502a5ea1490f2c1d2
saintjin0 <byoungikhwang@gmail.com>
2025-11-17 18:05:10 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,18 @@
+first = 4
+
+# if first >= 5:
+#     print("welcome to the rollercoaster!")
+#     print("it's me!")
+
+second = 6
+
+
+first = int(input("input first munber: "))
+second = int(input("input second munber: "))
+
+if first >= second:
+     print("welcome to the rollercoaster!")
+     print("it's me!")
+
+print("end of program")
+
