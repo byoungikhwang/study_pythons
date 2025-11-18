6a8d57ad5cf5aef8a57d6a07bda6daab0a454501
saintjin0 <byoungikhwang@gmail.com>
2025-11-18 15:00:25 +0900

    Fix: IndentationError in 05_ifs_whiles.py

--- codes/05_ifs_whiles.py
+++ codes/05_ifs_whiles.py
@@ -1,6 +1,10 @@
 first = 5   # 오타
 
-while first > 0:
-    print(f"while 값 : {first}")
-    first = first - 1
+# while first > 0:
+#    print(f"while 값 : {first}")
+#    first = first - 1
 
+
+while first > 0:
+    print(f"while 값 : {first}")  # 들여쓰기 문제
+    first = first - 1
