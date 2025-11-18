314942909dd2fc3cef232650ab74c3f90ab7563c
saintjin0 <byoungikhwang@gmail.com>
2025-11-18 17:12:13 +0900

    05_ifs.py

--- codes/05_ifs.py
+++ codes/05_ifs.py
@@ -1,18 +1,30 @@
+
 first = 4
 
 # if first >= 5:
 #     print("welcome to the rollercoaster!")
 #     print("it's me!")
 
-second = 6
+#second = 6
 
 
-first = int(input("input first munber: "))
-second = int(input("input second munber: "))
+#first = int(input("input first munber: "))
+#second = int(input("input second munber: "))
 
-if first >= second:
-     print("welcome to the rollercoaster!")
-     print("it's me!")
+#if first >= second:
+#     print("welcome to the rollercoaster!")
+#     print("it's me!")
 
-print("end of program")
+#print("end of program")
+
+##  다중 if-elif-else 문
+age = int(input("enter your age: ")) # 숫자 값으로 변화해서 받는다.
+
+if age < 4: 
+     print("your ticket is free")
+elif age < 18:
+     print("your ticket is $5") 
+else:
+     print("your ticket is $10")
+
 
