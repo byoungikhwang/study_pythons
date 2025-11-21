26d2d71f3dd27c4f09a881c0a65bd0bec4cdb5c4
saintjin0 <byoungikhwang@gmail.com>
2025-11-21 13:23:00 +0900

    classes_fourcals.py

--- codes/classes_fourcals.py
+++ codes/classes_fourcals.py
@@ -1,5 +1,14 @@
-#class class_name():
+# class class_name():
+# 초기화
 class FourCal:
+    first = 0 
+    second = 0  
+
+    def __init__(self):
+        self.first = 4
+        self.second = 6
+        pass
+
     def add(self, a, b):
         return a + b
     
@@ -16,9 +25,10 @@ class FourCal:
 
 if __name__ == "__main__":
     fourcal = FourCal()
-    
     print(f"3 + 5 = {fourcal.add(3, 5)}")
     print(f"10 - 4 = {fourcal.sub(10, 4)}")
     print(f"7 * 8 = {fourcal.mul(7, 8)}")
     print(f"20 / 5 = {fourcal.div(20, 5)}")
     print(f"10 / 0 = {fourcal.div(10, 0)}")
+
+    pass
